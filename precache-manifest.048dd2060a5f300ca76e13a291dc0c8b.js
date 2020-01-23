435cf050397c4b65d844a3be8864fc754e7e32f1
Dustin heaps <dustinheaps89@gmail.com>
2020-01-22 19:09:25 -0500

    Updates

---
+++ precache-manifest.048dd2060a5f300ca76e13a291dc0c8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1a957a7d1628028138d1a71222ee93",
+    "url": "/triviaApp/index.html"
+  },
+  {
+    "revision": "129a53ef9830ae5b66e8",
+    "url": "/triviaApp/static/css/2.ac9074d9.chunk.css"
+  },
+  {
+    "revision": "d205d88cc1d6410edd50",
+    "url": "/triviaApp/static/css/main.0eecf4ba.chunk.css"
+  },
+  {
+    "revision": "129a53ef9830ae5b66e8",
+    "url": "/triviaApp/static/js/2.6eb0bced.chunk.js"
+  },
+  {
+    "revision": "d205d88cc1d6410edd50",
+    "url": "/triviaApp/static/js/main.00e823af.chunk.js"
+  },
+  {
+    "revision": "5479a3fe12a15606b283",
+    "url": "/triviaApp/static/js/runtime~main.7f61664b.js"
+  },
+  {
+    "revision": "b70c56a3d47f70560d291d3dee2aa6d3",
+    "url": "/triviaApp/static/media/Background.b70c56a3.png"
+  }
+]);
